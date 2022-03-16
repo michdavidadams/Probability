aa44eb42456ffb22b712c76c5ecb90c9d4deaf8c
Michael Adams <31296356+michdavidadams@users.noreply.github.com>
2022-03-16 11:55:18 -0400

    Initial Commit

---
+++ Probability/ProbabilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProbabilityApp.swift
+//  Probability
+//
+//  Created by Michael Adams on 3/16/22.
+//
+
+import SwiftUI
+
+@main
+struct ProbabilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
